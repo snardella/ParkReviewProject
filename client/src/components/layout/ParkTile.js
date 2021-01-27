3a7ba3d05df26cd2f6e5ea0788843dd53ee2b2f5
magowntown <amagown@gmail.com>
2021-01-27 15:24:57 -0500

    cleaned code

--- client/src/components/layout/ParkTile.js
+++ client/src/components/layout/ParkTile.js
@@ -4,18 +4,10 @@ const ParkTile = ({id, name, location, description, rating, picture}) => {
   return(
     <div>
       <h3>{name}</h3>
-      <h5>
-        {location}
-      </h5>
-      <p>
-        {description}
-      </p>
-      <p>
-        {rating}
-      </p>
-      <p>
-        {picture}
-      </p>
+      <h5>{location}</h5>
+      <p>{description}</p>
+      <p>{rating}</p>
+      <p>{picture}</p>
     </div>
   )
 }
