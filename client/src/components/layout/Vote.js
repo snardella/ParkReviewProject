315d2017edb2099daee15855b7de89ddd753d18c
snardella <sam.nardella@gmail.com>
2021-02-05 09:53:46 -0500

    fixed vote bug

--- client/src/components/layout/Vote.js
+++ client/src/components/layout/Vote.js
@@ -1,67 +1,65 @@
 import React, { useState } from "react";
 
-
 const Vote = (props) => {
-  //debugger
-  // Initial state of the component.
-  let upVoteClass = undefined
-  let downVoteClass= undefined
-  
+  let upVoteClass = undefined;
+  let downVoteClass = undefined;
+
   const toggleUpvote = async (event) => {
-    
-    event.preventDefault()
-    if(props.currentVote.userId !== props.user.id){
+    event.preventDefault();
+    if (props.currentVote.userId !== props.user.id && props.parkId !== props.currentVote.parkId) {
       await props.postVote({
         vote: 1,
         parkId: props.parkId,
-      })
-      upVoteClass = "active"
-    }
-    else if(props.currentVote.userId === props.user.id && props.currentVote.voteTotal === 1){
-      alert("You cannot vote twice in a row!")
+      });
+      upVoteClass = "active";
+    } else if (
+      props.currentVote.userId === props.user.id &&
+      props.currentVote.voteTotal === 1 &&
+      props.parkId === props.currentVote.parkId
+    ) {
+      alert("You cannot vote twice in a row!");
     } else {
       await props.postVote({
         vote: 1,
         parkId: props.parkId,
-      })
-      upVoteClass = "active"
+      });
+      upVoteClass = "active";
     }
-  }
+  };
 
   const toggleDownvote = async (event) => {
-    event.preventDefault()
-    if(props.currentVote.userId !== props.user.id){
+    event.preventDefault();
+    if (props.currentVote.userId !== props.user.id && props.parkId !== props.currentVote.parkId) {
       await props.postVote({
         vote: -1,
         parkId: props.parkId,
-      })
-      downVoteClass = "active"
-    }
-    else if(props.currentVote.userId === props.user.id && props.currentVote.voteTotal === -1){
-      alert("You cannot vote twice in a row!")
+      });
+      downVoteClass = "active";
+    } else if (
+      props.currentVote.userId === props.user.id &&
+      props.currentVote.voteTotal === -1 &&
+      props.parkId === props.currentVote.parkId
+    ) {
+      alert("You cannot vote twice in a row!");
     } else {
       await props.postVote({
         vote: -1,
         parkId: props.parkId,
-      })
-      downVoteClass = "active"
+      });
+      downVoteClass = "active";
     }
-  }
+  };
 
   return (
-      <main>
-        <button
-          id="upvote"
-          className={upVoteClass}
-          onClick={toggleUpvote}
-        >Upvote</button>
-        <button
-          id="downvote"
-          className={downVoteClass}
-          onClick={toggleDownvote}
-        >Downvote</button>
-      </main>
-    );
-  }
+    <main>
+      <button id="upvote" className={upVoteClass} onClick={toggleUpvote}>
+        Upvote
+      </button>
+      <button id="downvote" className={downVoteClass} onClick={toggleDownvote}>
+        Downvote
+      </button>
+    </main>
+  );
+};
 
-export default Vote
+export default Vote;
